0348e6e6994d4abf6db4789e187ca206fa917ee5
D-Kode <74250461+Cl0udCode@users.noreply.github.com>
2021-12-06 15:10:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,125 +1,135 @@
-var readlineSync = require("readline-sync");
-var chalk = require("chalk")
+
+var Readlinesync = require("readline-sync");
+const chalk = require('chalk');
 
 
-//List of questions
-var quizList = [
-  {
-    question: "In what city is Saul working (as a Cinnabon manager) at the start of episode 1? ",
-    options: ["Orange County", "Omaha", "Illinois"],
-    answer: 1
-  },
-  {
-    question: "Who said, \"Slippin' Jimmy with a law degree is like a chimp with a machine gun?\" ",
-    options: ["Chuck", "Nacho", "Mike"],
-    answer: 0
-  },
-  {
-    question: "What is \"Nacho's\" real name? ",
-    options: ["Ignacio Varga", "Hector Salamanca", "Howard Hamlin"],
-    answer: 0
-  },
-  {
-    question: "Where were the Kettlemans hiding the stolen money? ",
-    options: ["Behind a wall", "Under their mattress", "Under the bathroom sink"],
-    answer: 2
-  },
-  {
-    question: "In what city did Mike work as a police officer? ",
-    options: ["Boston", "Philadelphia", "Nantucket"],
-    answer: 0
-  },
-  {
-    question: "From what university did Jimmy McGill receive his law degree? ",
-    options: ["University of New Mexico", "Harvard University", "University of American Samoa"],
-    answer: 2
-  },
-  {
-    question: "What does Chuck \"suffer\" from? ",
-    options: ["Light sensitivity", "Cryptophobia", "Electromagnetic hypersensitivity"],
-    answer: 2
-  },
-  {
-    question: "What is Saul Goodman's real name? ",
-    options: ["James Morgan McGill", "James Jones McGill", "Jeffrey James McGill"],
-    answer: 0
-  },
-  {
-    question: "Who is the dealer on the other side of Pryce's first drug deal (for which Mike acts as bodyguard)? ",
-    options: ["Tuco", "No-Doze", "Nacho"],
-    answer: 2
-  },
-  {
-    question: "Which nursing home does Jimmy make his first big case? ",
-    options: ["Sandpiper Crossing", "Home of Old Hearts", "Alberquerque Seniors Center"],
-    answer: 0
-  },
-]
 
-//Play each question
-function letsplay(question, options, answer){
-  var userAnswer = readlineSync.keyInSelect(options, question, {cancel: 'Exit'});
+ 
+const ques = chalk.bold.red;
+const ans = chalk.keyword('orange');
 
-  console.log("\n")
+userAnswer = Readlinesync.question("what is your name?")
 
+
+console.log("welcome "+ans(userAnswer)+' Welcome to ' + ques('Harry potter quiz') );
+console.log("__________________________________________________")
+
+
+
+
+ 
+
+
+
+
+
+
+function play(question,answer){
+  userAnswer = Readlinesync.question(ques(question) )
   if(userAnswer === answer){
-    console.log(chalk.bgGreen("S'ALL GOOD, MAN! Correct Answer!"));
-    score += 1
-  } else if(userAnswer === -1){
-    console.log(chalk.bgYellow("Sorry to see you leave! Exiting game..."));
-    return;
-  } else {
-    console.log(chalk.bgRed("Dont be a Slippin' Jimmy! Wrong Answer!"));
+   console.log("you are right 👌");
+   console.log("right answer is "+" -> "+ chalk.blue(answer) + "👏");
+    
+  }
+  else {
+   console.log("you are wrong 😭");
+   console.log("right answer is "+" -> "+ chalk.blue(answer)+"🤷‍♂️");
+
   }
 
-  console.log("Your sentencing count: " + chalk.magenta(score));
-  console.log("----------------------------------------");
+
 
 }
 
-//Score counter
-var score = 0;
+const question = [
 
-//Scoreboard
-var scoreBoard = [
+ {
+    question: "who wrote harry potter series?",
+
+    answer: "J. K. Rowling"
+
+  
+
+
+}, 
   {
-    name: "Omar",
-    score: 7
-  },
-  {
-    name: "Namya",
-    score: 4
-  },
-  {
-    name: "George",
-    score: 3
-  }
+    question: "What spell did Harry use to kill Lord Voldemort?",
+
+    answer: "Expelliarmus"
+
+
+},
+{
+    question: "It’s Levi-O-sa, not…",
+
+    answer: "Levi-o-SA"
+
+  
+
+
+},
+{
+    question: "Which Patronus belongs to Luna Lovegood?",
+
+    answer: "Rabbit"
+
+  
+
+
+},
+{
+    question: "The tears of which animal are the only known antidote to basilisk venom?",
+
+    answer: "Phoenix"
+
+  
+
+
+},
+{
+    question: "What position does Harry play on his Quidditch team?",
+
+    answer: "Seeker"
+
+  
+
+
+},
+{
+    question: "Who knocks out the troll in the ladies’ bathroom in Harry Potter and the Philosopher’s Stone?",
+
+    answer: "Ron"
+
+  
+
+
+},
+{
+    question: "Who poses as Mad-Eye Moody, Harry’s 4th year Defense Against the Dark Arts professor?",
+
+    answer: "Barty Crouch Jr."
+
+  
+
+
+},
+{
+    question: "What magical talent does Harry share with Voldemort?",
+
+    answer: "Being a Parselmouth"
+
+  
+
+
+},
+
 ]
 
-//introduction of game
-var playerName = readlineSync.question("What's your name? ");
-console.log(chalk.bgCyan("Welcome " + playerName + " to the BETTER CALL SAUL QUIZ!!"));
-console.log(chalk.bold("*Song from Little Barrie's album plays....*"));
-console.log("----------------------------------------");
-console.log("\n");
-console.log("This quiz contains 10 questions covering all 5 seasons of Better Call Saul. Do you have what it takes to answer them all?");
-console.log("LET'S BEGIN");
+for(i=0;i<question.length;i++){
 
-//Loop through each question
-for(var i=0; i<quizList.length; i++){
-  letsplay(quizList[i].question, quizList[i].options, quizList[i].answer)
+ play(question[i].question,question[i].answer);
+
+ console.log("__________________________________________________")
+
 }
 
-//end of game
-console.log("\n")
-console.log("YAY!! You scored " + score + " points!")
-console.log("\n")
-console.log("How did you do? Check out the scoreboard below!");
-console.log("If you got a highscore, ping me the proof and I'll update it here!!");
-
-//display scoreboard
-console.log("\n")
-console.log("SCOREBOARD");
-for(var i=0; i<scoreBoard.length; i++){
-  console.log(scoreBoard[i].name + " : " + scoreBoard[i].score);
-}
